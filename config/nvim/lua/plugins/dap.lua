1d31e1504d99e1dabaaef3c8e5b970d1ff5af216
Cory Hutchison <cory.hutchison@shopify.com>
2022-05-04 07:43:30 -0600

    [nvim] cleanup invalid dap configs

--- config/nvim/lua/plugins/dap.lua
+++ config/nvim/lua/plugins/dap.lua
@@ -3,28 +3,28 @@
 
 local dap = require('dap')
 
-dap.adapters.lldb = {
-  type = 'executable';
-  command = '/opt/homebrew/Cellar/llvm/13.0.0_1/bin/lldb-vscode';
-  name = 'lldb';
-}
+-- dap.adapters.lldb = {
+--   type = 'executable';
+--   command = '/opt/homebrew/Cellar/llvm/13.0.0_1/bin/lldb-vscode';
+--   name = 'lldb';
+-- }
+--
+-- dap.adapters.python = {
+--   type = 'executable';
+--   command = os.getenv('HOME') .. '/.pyenv/shims/python';
+--   args = { '-m', 'debugpy.adapter' };
+-- }
+--
+-- dap.configurations.python = {
+--   {
+--     type = 'python';
+--     request = 'launch';
+--     name = "Launch file";
+--     program = "${file}";
+--     pythonPath = function()
+--       return os.getenv('HOME') .. '/.pyenv/shims/python'
+--     end;
+--   },
+-- }
 
-dap.adapters.python = {
-  type = 'executable';
-  command = os.getenv('HOME') .. '/.pyenv/shims/python';
-  args = { '-m', 'debugpy.adapter' };
-}
-
-dap.configurations.python = {
-  {
-    type = 'python';
-    request = 'launch';
-    name = "Launch file";
-    program = "${file}";
-    pythonPath = function()
-      return os.getenv('HOME') .. '/.pyenv/shims/python'
-    end;
-  },
-}
-
-require('dapui').setup{}
+require('dapui').setup({})
