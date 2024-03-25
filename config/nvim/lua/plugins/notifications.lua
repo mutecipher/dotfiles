bc508407acea937f7b610bbe7ede44b78f03eff1
Cory Hutchison <cory@mutecipher.com>
2024-03-25 13:34:25 -0600

    Fix nvim-notify

--- config/nvim/lua/plugins/notifications.lua
+++ config/nvim/lua/plugins/notifications.lua
@@ -1,13 +1,22 @@
 return {
-  "folke/noice.nvim",
-  opts = function(_, opts)
-    table.insert(opts.routes, {
-      background_colour = "#000000",
-      filter = {
-        event = "notify",
-        find = "No information available",
-      },
-      opts = { skip = true },
-    })
-  end,
+  {
+    "folke/noice.nvim",
+    opts = function(_, opts)
+      table.insert(opts.routes, {
+        filter = {
+          event = "notify",
+          find = "No information available",
+        },
+        opts = { skip = true },
+      })
+    end,
+  },
+  {
+    "rcarriga/nvim-notify",
+    config = function()
+      require("notify").setup({
+        background_colour = "#000000",
+      })
+    end,
+  },
 }
