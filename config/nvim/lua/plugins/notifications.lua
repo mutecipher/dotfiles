f616e42e5d77e05790fabcd1afeb3f33c12ad7a9
Cory Hutchison <cory@mutecipher.com>
2024-03-25 11:03:47 -0600

    Tweaks, tweaks and more tweaks

--- config/nvim/lua/plugins/notifications.lua
+++ config/nvim/lua/plugins/notifications.lua
@@ -2,6 +2,7 @@ return {
   "folke/noice.nvim",
   opts = function(_, opts)
     table.insert(opts.routes, {
+      background_colour = "#000000",
       filter = {
         event = "notify",
         find = "No information available",
