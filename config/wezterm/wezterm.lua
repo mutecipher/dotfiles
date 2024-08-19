3092dbececbb5851f2d0d43e7f8930ac4ab49636
Cory Hutchison <cory@mutecipher.com>
2024-08-18 23:38:40 -0600

    Clean up Wezterm configs

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,21 +1,48 @@
 local wezterm = require("wezterm")
 local appearance = require("appearance")
-local github = require("github_theme")
 
 local config = wezterm.config_builder()
 
-config.color_schemes = github.color_schemes
-
 if appearance.is_dark() then
-	config.color_scheme = "GitHub Dark Default"
+	config.color_scheme = "Github Dark Default"
 else
-	config.color_scheme = "GitHub Light Default"
+	config.color_scheme = "Github Light Default"
 end
 
-config.font = wezterm.font("MonoLisa Variable")
+config.font = wezterm.font({
+	family = "MonoLisa Variable",
+	harfbuzz_features = { "calt=1", "liga=1", "zero=1", "ss02=1", "ss06=1", "ss07=1", "ss08=1", "ss10=1", "ss11=1" },
+})
 config.font_size = 14
 
 config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
+
+config.set_environment_variables = {
+	PATH = "/opt/homebrew/bin:" .. os.getenv("PATH"),
+}
+
+config.keys = {
+	-- Sends ESC + b and ESC + f sequence, which is used
+	-- for telling your shell to jump back/forward.
+	{
+		key = "LeftArrow",
+		mods = "OPT",
+		action = wezterm.action.SendString("\x1bb"),
+	},
+	{
+		key = "RightArrow",
+		mods = "OPT",
+		action = wezterm.action.SendString("\x1bf"),
+	},
+	{
+		key = ",",
+		mods = "SUPER",
+		action = wezterm.action.SpawnCommandInNewTab({
+			cwd = wezterm.home_dir,
+			args = { "nvim", wezterm.config_file },
+		}),
+	},
+}
 
 return config
