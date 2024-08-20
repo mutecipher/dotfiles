b671d53eb2c7d11765edd050a02d4f7529888c3c
Cory Hutchison <cory@mutecipher.com>
2024-08-19 23:03:07 -0600

    Add stylua to wezterm configs

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -4,14 +4,14 @@ local appearance = require("appearance")
 local config = wezterm.config_builder()
 
 if appearance.is_dark() then
-	config.color_scheme = "Github Dark Default"
+  config.color_scheme = "Github Dark Default"
 else
-	config.color_scheme = "Github Light Default"
+  config.color_scheme = "Github Light Default"
 end
 
 -- Modify the PATH to include Homebrew binaries
 config.set_environment_variables = {
-	PATH = "/opt/homebrew/bin:" .. os.getenv("PATH"),
+  PATH = "/opt/homebrew/bin:" .. os.getenv("PATH"),
 }
 
 config.initial_cols = 120
@@ -20,18 +20,18 @@ config.initial_rows = 40
 config.native_macos_fullscreen_mode = true
 
 config.font = wezterm.font({
-	family = "MonoLisa Variable",
-	harfbuzz_features = {
-		"calt=1", -- Whitespace ligatures
-		"liga=1", -- Enable ligatures
-		"zero=1", -- Zeros with lines instead of dot
-		"ss02=1", -- Script variant of italics
-		"ss06=1", -- Proper @ symbol
-		"ss07=1", -- Curlier braces
-		"ss08=1", -- Rounder parentheses
-		"ss10=1", -- Alternative greater than or equal symbol (eg. >=)
-		"ss11=1", -- Alternative hexadecimal symbol (eg. 0x23)
-	},
+  family = "MonoLisa Variable",
+  harfbuzz_features = {
+    "calt=1", -- Whitespace ligatures
+    "liga=1", -- Enable ligatures
+    "zero=1", -- Zeros with lines instead of dot
+    "ss02=1", -- Script variant of italics
+    "ss06=1", -- Proper @ symbol
+    "ss07=1", -- Curlier braces
+    "ss08=1", -- Rounder parentheses
+    "ss10=1", -- Alternative greater than or equal symbol (eg. >=)
+    "ss11=1", -- Alternative hexadecimal symbol (eg. 0x23)
+  },
 })
 config.font_size = 13
 
@@ -39,26 +39,26 @@ config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
 
 config.keys = {
-	-- Sends ESC + b and ESC + f sequence, which is used
-	-- for telling your shell to jump back/forward.
-	{
-		key = "LeftArrow",
-		mods = "OPT",
-		action = wezterm.action.SendString("\x1bb"),
-	},
-	{
-		key = "RightArrow",
-		mods = "OPT",
-		action = wezterm.action.SendString("\x1bf"),
-	},
-	{
-		key = ",",
-		mods = "SUPER",
-		action = wezterm.action.SpawnCommandInNewTab({
-			cwd = wezterm.home_dir,
-			args = { "nvim", wezterm.config_file },
-		}),
-	},
+  -- Sends ESC + b and ESC + f sequence, which is used
+  -- for telling your shell to jump back/forward.
+  {
+    key = "LeftArrow",
+    mods = "OPT",
+    action = wezterm.action.SendString("\x1bb"),
+  },
+  {
+    key = "RightArrow",
+    mods = "OPT",
+    action = wezterm.action.SendString("\x1bf"),
+  },
+  {
+    key = ",",
+    mods = "SUPER",
+    action = wezterm.action.SpawnCommandInNewTab({
+      cwd = wezterm.home_dir,
+      args = { "nvim", wezterm.config_file },
+    }),
+  },
 }
 
 return config
