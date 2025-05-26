38acb1f25bcf0d2c005dda1a6d1915ecc37fa6c3
Cory Hutchison <cory@mutecipher.com>
2025-05-25 22:08:41 -0600

    Replace Terraform Plugin With Tofu Formatter
    Remove the Terraform language plugin from LazyVim configuration and add
    support for formatting Terraform, HCL, and TF files using the tofu
    formatter via conform.nvim.

--- config/nvim/lua/plugins/builtin.lua
+++ config/nvim/lua/plugins/builtin.lua
@@ -12,4 +12,21 @@ return {
       })
     end,
   },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        tf = { "tfmt" },
+        terraform = { "tfmt" },
+        hcl = { "tfmt" },
+      },
+      formatters = {
+        tfmt = {
+          command = "tofu",
+          args = { "fmt", "-" },
+          stdin = true,
+        },
+      },
+    },
+  },
 }
