03c54566668d53026f347b584f2935565a1cd1d4
Cory Hutchison <cory@mutecipher.com>
2026-02-24 18:05:17 -0700

    Add markdown-mode and tree-sitter configurations

--- config/emacs/lisp/plugin.el
+++ config/emacs/lisp/plugin.el
@@ -10,8 +10,6 @@
 	("melpa" . 2)
 	("nongnu" . 1)))
 
-;; Packages
-
 ;; Help learning what key combos do
 (use-package which-key
   :diminish
@@ -67,5 +65,29 @@
   :config
   (ligature-set-ligatures 't monolisa-ligatures)
   (global-ligature-mode t))
+
+;; Markdown support
+(use-package markdown-mode
+  :ensure t)
+
+(setq treesit-language-source-alist
+   '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+     (cmake "https://github.com/uyha/tree-sitter-cmake")
+     (css "https://github.com/tree-sitter/tree-sitter-css")
+     (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+     (go "https://github.com/tree-sitter/tree-sitter-go")
+     (html "https://github.com/tree-sitter/tree-sitter-html")
+     (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
+     (json "https://github.com/tree-sitter/tree-sitter-json")
+     (make "https://github.com/alemuller/tree-sitter-make")
+     (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+     (python "https://github.com/tree-sitter/tree-sitter-python")
+     (toml "https://github.com/tree-sitter/tree-sitter-toml")
+     (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
+     (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
+     (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
+
+
+(mapc #'treesit-install-language-grammar (mapcar #'car treesit-language-source-alist))
 
 (provide 'plugin)
